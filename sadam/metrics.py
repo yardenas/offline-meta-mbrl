90c83e9bb69d90216012f4a361d8545304da85d0
Yarden <yarden.as@inf.ethz.ch>
2023-03-28 13:17:28 +0200

    Fix bug with running stats

--- sadam/metrics.py
+++ sadam/metrics.py
@@ -13,9 +13,14 @@ class Metrics(NamedTuple):
 
 class MetricsAccumulator:
     def __init__(self):
-        self._state = None
+        self._state = Metrics(
+            np.zeros((1,)),
+            np.ones((1,)),
+            np.empty((1,)),
+            np.empty((1,)),
+        )
         self._count = 0
-        self._m2 = None
+        self._m2 = 0.0
 
     def update_state(
         self, sample: float | npt.NDArray[Any], axis: int | Sequence[int] = 0
@@ -26,41 +31,35 @@ class MetricsAccumulator:
                     sample,
                 ]
             )
-        new_mean = sample.mean(axis=axis)
-        new_std = sample.std(axis=axis)
-        new_min = sample.min(axis=axis)
-        new_max = sample.max(axis=axis)
-        if self._state is None:
-            self._state = Metrics(new_mean, new_std, new_min, new_max)
-        else:
-            delta = new_mean - self._state.mean
-            updated_mean = self._state.mean + delta / self._count
-            delta2 = new_mean - updated_mean
-            self._m2 = (
-                self._m2 + delta2 * delta2 if self._m2 is not None else delta2**2
-            )
-            self._state = Metrics(
-                updated_mean,
-                self._m2 / (self._count - 1) if self._count > 1 else new_std,
-                np.minimum(self._state.min, new_min),
-                np.maximum(self._state.max, new_max),
-            )
+        batch_mean = sample.mean(axis=axis)
+        batch_var = sample.std(axis=axis)
+        batch_min = sample.min(axis=axis)
+        batch_max = sample.max(axis=axis)
         self._count += 1
+        delta = batch_mean - self._state.mean
+        new_mean = self._state.mean + delta / self._count
+        delta2 = batch_mean - new_mean
+        self._m2 = self._m2 + delta * delta2
+        new_stddev = np.sqrt(
+            self._m2 / (self._count - 1) if self._count > 1 else batch_var
+        )
+        self._state = Metrics(
+            new_mean,
+            new_stddev,
+            np.minimum(self._state.min if self._count == 1 else batch_min, batch_min),
+            np.maximum(self._state.max if self._count == 1 else batch_max, batch_max),
+        )
 
     @property
     def result(self):
-        if self._state is not None:
-            state = self._state
-        else:
-            state = Metrics(
-                np.zeros((1,)),
-                np.ones((1,)),
-                -np.inf * np.ones((1,)),
-                np.inf * np.ones((1,)),
-            )
-        return state
+        return self._state
 
     def reset_states(self):
-        self._state = None
+        self._state = Metrics(
+            np.zeros((1,)),
+            np.ones((1,)),
+            np.zeros((1,)),
+            np.zeros((1,)),
+        )
         self._count = 0
-        self._m2 = None
+        self._m2 = 0.0
