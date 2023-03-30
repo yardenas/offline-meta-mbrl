341d36e5ad7ce4f580c31e8205e87b108176353f
Yarden <yarden.as@inf.ethz.ch>
2023-03-30 08:50:22 +0200

    Add model training test

--- tests/test_training.py
+++ tests/test_training.py
@@ -40,3 +40,52 @@ def test_training():
 
     with Trainer(cfg, make_env, task_sampler) as trainer:
         trainer.train(epochs=1)
+
+
+def test_model_learning():
+    import numpy as np
+
+    with initialize(version_base=None, config_path="../sadam/"):
+        cfg = compose(
+            config_name="config",
+            overrides=[
+                "training.time_limit=32",
+                "training.episodes_per_task=1",
+                "training.task_batch_size=5",
+                "training.parallel_envs=5",
+                "sadam.model.n_layers=1",
+                "sadam.model.hidden_size=32",
+                "sadam.model.hippo_n=8",
+                "sadam.update_steps=1",
+                "sadam.replay_buffer.sequence_length=16",
+            ],
+        )
+    if not cfg.training.jit:
+        from jax.config import config as jax_config  # pyright: ignore
+
+        jax_config.update("jax_disable_jit", True)
+
+    def make_env():
+        import gymnasium as gym
+
+        env = gym.make("Pendulum-v1")
+        env._max_episode_steps = cfg.training.time_limit  # type: ignore
+
+        return env
+
+    def task_sampler(dummy: int, dummy2: Optional[bool] = False) -> Iterable[int]:
+        for _ in range(cfg.training.task_batch_size):
+            yield 1
+
+    with Trainer(cfg, make_env, task_sampler) as trainer:
+        assert trainer.agent is not None and trainer.env is not None
+        from sadam.sadam import SAdaM
+
+        SAdaM.__call__ = lambda self, observation: np.tile(
+            trainer.env.action_space.sample(),
+            (
+                cfg.training.task_batch_size,
+                1,
+            ),
+        )
+        trainer.train(epochs=1)
